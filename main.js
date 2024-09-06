d61ca4e6c13ea7d56d42a568ed72a2438c54c3bb
IhopGT <heatsalt7@gmail.com>
2024-09-06 00:14:06 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+// js/main.js
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+        alert('Button clicked!');
+    });
+});
